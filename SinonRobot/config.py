5eddb605f97cef4ceecf5b561736c3c45a14003a
Alone X <95560268+TheAloneX@users.noreply.github.com>
2022-05-23 11:18:21 +0530

    Add files via upload

---
+++ SinonRobot/config.py
@@ -0,0 +1,107 @@
+
+
+# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
+import json
+import os
+
+
+def get_user_list(config, key):
+    with open("{}/SinonRobot/{}".format(os.getcwd(), config), "r") as json_file:
+        return json.load(json_file)[key]
+
+
+# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
+class Config(object):
+    LOGGER = True
+    # REQUIRED
+    # Login to https://my.telegram.org and fill in these slots with the details given by it
+
+    API_ID = 18641799
+    API_HASH = "2027706706fd39baf84c01ff5b95a6a6"
+
+    BOT_ID = 5355063405
+    TOKEN = "5355063405:AAF8_7JXBQQm0lBFHz_dFpEBIMhD2vddTfM"
+    OWNER_ID = 5259108841 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "ItzAloneX"
+    DRAGONS = ""
+    DEMONS = ""
+    DEV_USERS = ""
+    SUPPORT_CHAT = "SinonSupport"  # Your own group for support, do not add the @
+    JOIN_LOGGER = (
+        -1001791135075
+    )  # Prints any new group the bot is added to, prints just the name and ID.
+    EVENT_LOGS = (
+        -1001791135075
+    )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+
+    # RECOMMENDED
+    SQLALCHEMY_DATABASE_URI = "postgres://zbrkaxqs:8JcU08L8AAcec6EEHOWdhoD80su3KIm5@arjuna.db.elephantsql.com/zbrkaxqs"  # needed for any database module
+    DATABASE_URL = "postgres://zbrkaxqs:8JcU08L8AAcec6EEHOWdhoD80su3KIm5@arjuna.db.elephantsql.com/zbrkaxqs"
+    DB_URL = "postgres://zbrkaxqs:8JcU08L8AAcec6EEHOWdhoD80su3KIm5@arjuna.db.elephantsql.com/zbrkaxqs"
+
+    REDIS_URI = ""
+
+
+    LOAD = []
+    NO_LOAD = []
+    WEBHOOK = False
+    INFOPIC = True
+    URL = None
+    HEROKU_API_KEY = ""
+    HEROKU_APP_NAME = ""
+    BOT_USERNAME = "SinonRobot"
+    SPAMWATCH_API = 'fIXLV8R4OQeAqy8mdVmLU34WT8gZHde4iTuOhh4QLXpjZYxVyU4C~jq40inaykJk'  # go to support.spamwat.ch to get key
+    SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
+    ARQ_API_KEY = "1234"
+    ARQ_API_URL = "https://thearq.tech/"
+    TEMP_DOWNLOAD_DIRECTORY = "./"
+    OPENWEATHERMAP_ID = ""
+    VIRUS_API_KEY = ""
+    REDIS_URL = ""
+    LASTFM_API_KEY = ""
+    
+
+    # OPTIONAL
+    ##List of id's -  (not usernames) for users which have sudo access to the bot.
+    ALLOW_CHATS = ""
+    DRAGONS = get_user_list("elevated_users.json", "sudos")
+    WHITELIST_USERS = get_user_list("elevated_users.json", "whitelists")
+    ##List of id's - (not usernames) for developers who will have the same perms as the owner
+    DEV_USERS = get_user_list("elevated_users.json", "devs")
+    ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    DEMONS = get_user_list("elevated_users.json", "supports")
+    # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    TIGERS = get_user_list("elevated_users.json", "tigers")
+    WOLVES = get_user_list("elevated_users.json", "whitelists")
+    DONATION_LINK = None  # EG, paypal
+    CERT_PATH = None
+    PORT = 5000
+    DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
+    STRICT_GBAN = True
+    WORKERS = (
+        8  # Number of subthreads to use. Set as number of threads your processor uses
+    )
+    BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
+    ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
+    CASH_API_KEY = (
+        "xyz"  # Get your API key from https://www.alphavantage.co/support/#api-key
+    )
+    TIME_API_KEY = "xyz"  # Get your API key from https://timezonedb.com/api
+    WALL_API = (
+        "xyz"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
+    )
+    AI_API_KEY = ""  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
+    BL_CHATS = []  # List of groups that you want blacklisted.
+    SPAMMERS = None
+    REM_BG_API_KEY = ""
+    GENIUS_API_TOKEN = ""
+    MONGO_DB_URI = "mongodb+srv://AloneX:AloneX@cluster0.0hh7x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    STRING_SESSION = ""
+    
+
+class Production(Config):
+    LOGGER = True
+
+
+class Development(Config):
+    LOGGER = True
---
+++ SinonRobot/sinontext.py
@@ -0,0 +1,7 @@
+PM_START_TEXT = """
+*────「* [{}](https://telegra.ph/file/9ce8d2ef564eaab4bb104.jpg) *」────*
+*Hey {}, I Am Sinon,
+I Am An Anime Themed Advance Group Management Bot With Lots Of Speciality.*
+*➖➖➖➖➖➖➖➖➖➖➖➖➖
+◈ Try The Help Buttons Below To Know My Abilities ♡*
+"""
