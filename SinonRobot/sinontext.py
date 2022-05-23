8399e361654b1c07f48e2ad7366964544f11f910
Alone X <95560268+TheAloneX@users.noreply.github.com>
2022-05-23 12:07:07 +0530

    Sinon • Robot

--- SinonRobot/sinontext.py
+++ SinonRobot/sinontext.py
@@ -5,3 +5,28 @@ I Am An Anime Themed Advance Group Management Bot With Lots Of Speciality.*
 *➖➖➖➖➖➖➖➖➖➖➖➖➖
 ◈ Try The Help Buttons Below To Know My Abilities ♡*
 """
+
+SINON_IMG = (
+      "https://telegra.ph/file/0f6db348c0d63f5a74160.jpg",
+      "https://telegra.ph/file/fb178894409b54db8ccf0.jpg",
+      "https://telegra.ph/file/12d50d9d064d6a985dabe.jpg",
+      "https://telegra.ph/file/7d3bf8bbf7e8208ec179d.jpg",
+      "https://telegra.ph/file/c64d8e2c4b72d45c61f60.jpg",
+      "https://telegra.ph/file/f9290f9fcf04da3dfe42e.jpg",
+)
+
+TEXXT = "<b>Heya</b> {} <b>Do you Like me?</b>"
+
+SINON_N_IMG = (
+      "https://telegra.ph/file/d6c152cd3bdb9a446c338.jpg",
+      "https://telegra.ph/file/ba8f14b3119948b4b1a8b.jpg",
+      "https://telegra.ph/file/605e0e2d93f9a338e2ea8.jpg",
+      "https://telegra.ph/file/4d1fee917b01de0df08ec.jpg",
+      "https://telegra.ph/file/23439f288d876973a3d77.jpg"
+
+)
+
+PM_PHOTO = "https://telegra.ph/file/1f11d35c2002ffac502f9.jpg"
+
+SINON_DISPACHER_PIC = "https://telegra.ph/file/49b30183c55c30d9c46c1.jpg"
+
